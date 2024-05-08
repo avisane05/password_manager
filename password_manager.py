a07aeadc77792ba05877b63de4a0867fcee180f0
avisane05 <avisane05.as@gmail.com>
2024-05-08 13:10:37 +0530

    code_0

--- password_manager.py
+++ password_manager.py
@@ -1,14 +1,38 @@
-master_pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+write_key()
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+# master_pwd = input("What is the master password? ")
+
+key = load_key() # + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
-    pass
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
-    pwd = input("Password: ")
+    passw = input("Password: ")
 
     with open('password.txt', 'a') as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + fer.encrypt(passw.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add password or view existing ones or quite (view/add/q): ").lower()
@@ -17,7 +41,7 @@ while True:
         break
 
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
         add()
     else:
